d7f6a663cfb4b533d530b86fecdb67cc061e4a39
programmerNizam <mdnizam12109@gmail.com>
2022-09-09 23:30:43 +0600

    Update Readme file

--- src/Pages/Reviews/ReviewCard.js
+++ src/Pages/Reviews/ReviewCard.js
@@ -5,7 +5,7 @@ const ReviewCard = ({ review }) => {
   const { name, city, img, text } = review;
   return (
     <div className="hover:shadow-2xl rounded-r-2xl hover:duration-700 bg-base-100 h-[350px]">
-      <div className="flex flex-col lg:flex-row gap-5 justify-center items-center min-h-[83%]">
+      <div className="flex flex-col lg:flex-row gap-5 justify-center items-center min-h-[80%]">
         <div className="flex-1">
           {img ? (
             <img src={img} alt="" className="w-full h-[291px] object-cover" />
@@ -21,7 +21,7 @@ const ReviewCard = ({ review }) => {
           <ReadMore limit={200}>{text}</ReadMore>
         </div>
       </div>
-      <div className="px-5 py-2 bg-gradient-to-r from-[#8D5CF6] to-[#5cacf6]">
+      <div className="px-5 py-2 bg-gradient-to-r from-[#8D5CF6] to-[#5cacf6] h-[19%]">
         <h3 className="text-2xl text-white">{name}</h3>
         <h5 className="text-white">{city}</h5>
       </div>
