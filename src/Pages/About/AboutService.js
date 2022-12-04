6de17d680e78247b6a756c000a8ba3c75af79782
programmerNizam <mdnizam12109@gmail.com>
2022-12-04 16:36:27 +0600

    update about

--- src/Pages/About/AboutService.js
+++ src/Pages/About/AboutService.js
@@ -5,19 +5,21 @@ const AboutService = ({ service }) => {
   const { name, text, img } = service;
   return (
     <div>
-      <div className="shadow bg-base-100  p-10 rounded-lg group hover:bg-[#8962f680] hover:duration-1000 hover:scale-105">
+      <div className="shadow bg-base-100 p-10 rounded-lg group hover:glass hover:duration-700 hover:translate-y-[-5px]">
         <div className="text-center">
           <figure className="flex justify-center">
             <img
               src={img}
               alt=""
-              className="w-20 group-hover:bg-white p-5 rounded-full"
+              className="w-20 group-hover:bg-white group-hover:duration-700 p-5 rounded-full"
             />
           </figure>
-          <h2 className="text-2xl font-bold my-5 group-hover:text-white">
+          <h2 className="text-2xl font-bold my-5 group-hover:text-white group-hover:duration-700">
             {name}
           </h2>
-          <p className="group-hover:text-white">{text}</p>
+          <p className="group-hover:text-white group-hover:duration-700">
+            {text}
+          </p>
           <Link to={"/home"}>
             <button className="bg-primary py-2 px-5 text-white rounded mt-4">
               Learn more
